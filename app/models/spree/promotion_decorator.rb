5447619ca71d2e8b9289ead7b9246758a9359daa
nebirhos <nebirhos@aol.com>
2012-11-08 18:46:10 +0100

    Fix for resource create

--- app/models/spree/promotion_decorator.rb
+++ app/models/spree/promotion_decorator.rb
@@ -2,6 +2,6 @@ Spree::Promotion.class_eval do
 
   has_one :image, :as => :viewable, :dependent => :destroy, :class_name => "Spree::PromotionImage"
   accepts_nested_attributes_for :image, :reject_if => lambda { |i| i[:attachment].blank? && i[:_destroy] != "1" }, :allow_destroy => true
-  attr_accessible :image_attributes
+  attr_accessible :image_attributes, :image
 
 end
